13d6062e1a13f96ca70a1f9d388cedba5990ac69
Mc <50912656+gyq970927@users.noreply.github.com>
2021-01-07 16:53:36 +0800

    Add files via upload

---
+++ Trial.py
@@ -0,0 +1,55 @@
+# 声明
+print("这是来自自学python文件中的各种学习程序，具体如下：")
+'''
+注释如下
+a = 10
+print(type(a))
+a = '谷一群'
+print(type(a))
+a = False
+print(type(a))
+b = ()         #元组
+print(type(b))
+c = []         #列表
+print(type(c))
+d = {}         #字典
+print(type(d))
+# 变量的命名规则
+# 1.以字母或者下划线_开头，且区分大小写
+# 2.python关键词不能做变量,关键词如下:
+# False               break               for                 not
+# None                class               from                or
+# True                continue            global              pass
+# __peg_parser__      def                 if                  raise
+# and                 del                 import              return
+# as                  elif                in                  try
+# assert              else                is                  while
+# async               except              lambda              with
+# await               finally             nonlocal            yield
+# 变量的算术运算+ - * / % **
+a = 9
+b = 2
+print(a+b)
+print(a/b)
+print(a*b)
+print(a**b)
+print(a%b) #取余数
+print(a//b) #相除取整
+# 比较运算
+print(a==b)
+print(a!=b)
+print(a>=b)
+print(a<=b)
+print(a>b)
+print(a<b)
+# 逻辑运算
+print(a==b and a!=b)  #and条件严格，必须都为真结果才为真
+print(a==b or a!=b)  #or条件不严格，一个为真结果即为真
+print(not a==b)  #not否定原来的真假
+# 赋值运算
+b **= a
+print(b)
+# 格式化输出
+name,age = 'Lily',17
+print('My name is %s,I am %d years old now,\nAnd I study in England.'%(name,age))'''
+# input exercise
